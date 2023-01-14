e86593d0ed1dc6288222f3c0b7d0dc0ca8005441
dagon derik farias dantas <dagonderik@gmail.com>
2023-01-14 17:48:15 -0300

    Changes the styling of the page adding responsiveness

--- src/pages/dogs/Dogs.tsx
+++ src/pages/dogs/Dogs.tsx
@@ -6,17 +6,17 @@ function Dogs({}) {
   const [dog, setDog] = useState("");
 
   return (
-    <div className="container">
-      <Header/>
-      <h1>Take you daily dose of dog</h1>
-      <div className="button-container">
-        <button onClick={() => handleClick({ setDog })}>Refresh</button>
+    <div className="dogs-container">
+      <div className="header-container">
+        <Header/>
+      </div>
+      <h1 className="dog-title">Take you daily dose of dog</h1>
+        <button className="dog-button" onClick={() => handleClick({ setDog })}>Refresh</button>
         {dog.includes("mp4") || dog.includes("webm") ? (
           <video className="dog-image" autoPlay loop src={`${dog}`}></video>
         ) : (
           <img className="dog-image" src={`${dog}`} />
         )}
-      </div>
     </div>
   );
 }
