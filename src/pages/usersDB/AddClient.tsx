248d15f5dcd152cbdfc7b8f57083f5ad9f7a639d
dagon derik farias dantas <dagonderik@gmail.com>
2023-01-14 19:19:13 -0300

    Changes the styling of the page

--- src/pages/usersDB/AddClient.tsx
+++ src/pages/usersDB/AddClient.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Header from "../../components/header";
 
 const AddClient = () => {
   const [name, setName] = useState("");
@@ -27,83 +28,83 @@ const AddClient = () => {
   };
 
   return (
-    <div className="columns mt-5">
-      <div className="column is-half">
-        <form onSubmit={saveUser}>
-          <div className="field">
-            <label className="label">Name</label>
-            <div className="control">
-              <input
-                type="text"
-                className="input"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Email</label>
-            <div className="control">
-              <input
-                type="text"
-                className="input"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Phone</label>
-            <div className="control">
-              <div className="select is-fullwidth">
-                <input
-                  type="text"
-                  className="input"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                  placeholder="Phone"
-                ></input>
+    <div className="users-container">
+      <div className="header-container">
+        <Header />
+        <div className="main-contain">
+          <div className="column is-half">
+            <form onSubmit={saveUser}>
+              <div className="field">
+                <label className="label label-users">Name</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Name"
+                  />
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">Address</label>
-            <div className="control">
-              <div className="select is-fullwidth">
-                <input
-                  type="text"
-                  className="input"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  placeholder="Address"
-                ></input>
+              <div className="field">
+                <label className="label label-users">Email</label>
+                <div className="control">
+                  <input
+                    type="email"
+                    className="input"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                  />
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">CPF</label>
-            <div className="control">
-              <div className="select is-fullwidth">
-                <input
-                  type="text"
-                  className="input"
-                  value={cpf}
-                  onChange={(e) => setCPF(e.target.value)}
-                  placeholder="CPF"
-                ></input>
+              <div className="field">
+                <label className="label label-users">Phone</label>
+                <div className="control">
+                  <input
+                    type="tel"
+                    pattern="\([0-9]{2}\)[0-9]{5}-[0-9]{4}"
+                    className="input"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    placeholder="(XX) XXXXX-XXXX"
+                  ></input>
+                </div>
               </div>
-            </div>
+              <div className="field">
+                <label className="label label-users">Address</label>
+                <div className="control">
+                  <input
+                    type="text"
+                    className="input"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    placeholder="Address"
+                  ></input>
+                </div>
+              </div>
+              <div className="field">
+                <label className="label label-users">CPF</label>
+                <div className="control">
+                  <input
+                    type="number"
+                    className="input"
+                    value={cpf}
+                    onChange={(e) => setCPF(e.target.value)}
+                    placeholder="CPF"
+                  ></input>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <button type="submit" className="button is-success">
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="field">
-            <div className="control">
-              <button type="submit" className="button is-success">
-                Save
-              </button>
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
