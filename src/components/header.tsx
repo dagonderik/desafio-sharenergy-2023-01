85aaf7f1bd0b6acda395fd9e64f28f38dbcf2b1b
dagon derik farias dantas <dagonderik@gmail.com>
2023-01-14 16:40:20 -0300

    Updates the design of the nav bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,80 @@
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {
+  AppstoreOutlined,
+  ContainerOutlined,
+  DesktopOutlined,
+  MailOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  GithubOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
-function Header() {
-  return (
-    <div >
-      <ul>
-        <li>
-          <Link to="/cats" >
-          Cats
-          </Link>
-        </li>
-        <li>
-          <Link to="/dogs" >
-          Dogs
-          </Link>
-        </li>
-        <li>
-          <Link to="/clients" >
-          Add Clients
-          </Link>
-        </li>
-        <li>
-          <Link to="/users" >
-          Users
-          </Link>
-        </li>
-      </ul>
-      <ul>
-        <li></li>
-      </ul>
-    </div>
-  );    
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
 }
+
+const items: MenuItem[] = [
+  getItem('Cats', '/cats', <GithubOutlined />),
+  getItem('Dogs', '/dogs', <DesktopOutlined />),
+  getItem('Add Clients', '/clients', <ContainerOutlined />),
+  getItem('Users', '/users', <ContainerOutlined />),
+];
+
+const Header: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = (_id: any) => {
+    switch (_id.key) {
+      case "/cats" :
+        navigate("/cats");
+        break;
+    case "/dogs" :
+        navigate("/dogs");
+        break;
+    case "/clients" :
+        navigate("/clients");
+        break;
+    case "/users" :
+        navigate("/users");
+        break;
+  }
+}
+
+  return (
+    <div>
+      <Menu
+        defaultSelectedKeys={[`${window.location.pathname}`]}
+        onClick={handleClick}
+        mode="horizontal"
+        theme="light"
+        inlineCollapsed={collapsed}
+        items={items}
+      />
+    </div>
+  );
+};
 
 export default Header;
