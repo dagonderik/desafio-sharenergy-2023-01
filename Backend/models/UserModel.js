567cbb391043b87593d610d966bc224ba8124d43
dagon derik farias dantas <dagonderik@gmail.com>
2023-01-16 11:00:45 -0300

    Adds backend folder to the project

---
+++ Backend/models/UserModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const User = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Users", User);
